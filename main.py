46b9fb3b6cda518e96fc3f824cec9ac0bb1bddd4
Stelmakh Bohdan <bodya300595@gmail.com>
2024-01-29 11:12:09 +0200

    add video converting method to binary type

--- main.py
+++ main.py
@@ -18,3 +18,17 @@ erode = cv2.erode(dilate, kernel,  iterations=1)
 cv2.imshow('Photo', erode)
 cv2.waitKey(0)
 
+
+# photo = np.zeros((600, 450, 3), dtype='uint8')
+
+# BGR from OpenCv = RGB
+# photo[100:150, 200:280] = 119, 201, 105
+
+# cv2.rectangle(photo, (0, 0), (100, 100), (119, 201, 105), thickness=3)
+# cv2.line(photo, (150, 150), (300, 150), (119, 201, 105), thickness=3)
+# cv2.circle(photo, (photo.shape[1] // 2, photo.shape[0] // 2), 50, (119, 201, 105), thickness=cv2.FILLED)
+#
+# cv2.putText(photo, 'TEXT', (300, 300), cv2.FONT_HERSHEY_TRIPLEX, 1, (119, 201, 105), 2)
+#
+# cv2.imshow('Photo', photo)
+# cv2.waitKey(0)
--- videoWork.py
+++ videoWork.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 capture = cv2.VideoCapture('video/car_video.mp4')
 # capture = cv2.VideoCapture(0)
@@ -8,7 +9,18 @@ capture.set(4, 400)
 
 while True:
     success, img = capture.read()
-    cv2.imshow('Video Result', img)
+
+    bw_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    new_img = cv2.Canny(bw_img, 30, 30)
+
+    kernel = np.ones((5, 5), np.uint8)
+
+    dilate = cv2.dilate(new_img, kernel, iterations=1)
+    erode = cv2.erode(dilate, kernel, iterations=1)
+
+    cv2.imshow('Video Result', erode)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
