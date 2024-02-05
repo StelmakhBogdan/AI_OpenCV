65c5494364fefa34544d8e262160abf993954388
Stelmakh Bohdan <bodya300595@gmail.com>
2024-02-05 16:28:04 +0200

    add part of code for detecting car numbers

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 
 import imutils
+import easyocr
+from matplotlib import pyplot as pl
 
 # from helpers.rotate import rotate
 
@@ -54,15 +56,43 @@ import imutils
 # cv2.waitKey(0)
 
 
-image = cv2.imread('images/people2.webp')
-grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# image = cv2.imread('images/people2.webp')
+# grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#
+# faces = cv2.CascadeClassifier('faces_neural_network.xml')
+#
+# result = faces.detectMultiScale(grey_img, scaleFactor=1.1, minNeighbors=8)
+#
+# for (x, y, w, h) in result:
+#     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+#
+# cv2.imshow('Neural Network result', image)
+# cv2.waitKey(0)
 
-faces = cv2.CascadeClassifier('faces_neural_network.xml')
+img = cv2.imread('images/gla.jpeg')
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_filter = cv2.bilateralFilter(gray_img, 11, 15, 15)
+edges = cv2.Canny(img_filter,  30, 200)
 
-result = faces.detectMultiScale(grey_img, scaleFactor=1.1, minNeighbors=8)
+cont = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cont = imutils.grab_contours(cont)
+# cont = sorted(cont, key=cv2.contourArea, reverse=True)[:8]
+cont = sorted(cont, key=cv2.contourArea, reverse=True)
 
-for (x, y, w, h) in result:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
+pos = None
+for c in cont:
+    approx = cv2.approxPolyDP(c, 10, True)
 
-cv2.imshow('Neural Network result', image)
-cv2.waitKey(0)
+    if len(approx) == 4:
+        pos = approx
+        break
+
+
+print(pos)
+
+mask = np.zeros(gray_img.shape, np.uint8)
+new_img = cv2.drawContours(mask, [pos], 0, 255, -1)
+bitwise_img = cv2.bitwise_and(img, img, mask=mask)
+
+pl.imshow(cv2.cvtColor(bitwise_img, cv2.COLOR_BGR2RGB))
+pl.show()
